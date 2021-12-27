6c11f0ddc22e67f5ca90380a29c6e94b4c2ffd75
perseu912 <perseu912@gmail.com>
2021-12-27 10:21:48 -0300

    v0.0.1012 fix to errorname lib in comand from shell setup

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open("README.md", "r") as fh:
     readme = fh.read()
 
 setup(name='tubemp3',
-    version='0.0.1',
-    url='https://github.com/perseu912/toddy',
+    version='0.0.1012',
+    url='https://github.com/perseu912/tubemp3',
     license='MIT License',
     author='Reinan Br',
     entry_points = {
-        'console_scripts': ['getmusic=tube.get_music_line:main'],
+        'console_scripts': ['getmusic=tubemp3.get_music_line:main'],
     },
     long_description=readme,
     long_description_content_type="text/markdown",
     author_email='slimchatuba@gmail.com',
     keywords='kit tools dev works',
-    description=u'Library for development with toolskit for more works📈📊📚',
+    description=u'Library for getting music in high quality from YouTube',
     packages=find_packages(),
     install_requires=['psutil','requests','googlesearch-python',
     'youtube-dl','mp3-tagger','stagger','eyed3'],)
